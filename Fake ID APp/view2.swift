b69047e6b703b00d273ea131fd8c9f65182b7a63
Trevor Antram <trevor2076@Shauns-MacBook-Pro.local>
2016-03-25 15:18:32 +0000

    This is the first working copy of the app with everything working.

---
+++ Fake ID APp/view2.swift
@@ -0,0 +1,18 @@
+//
+//  view2.swift
+//  Fake ID Final
+//
+//  Created by Trevor Antram on 3/15/16.
+//  Copyright © 2016 Trevor Antram. All rights reserved.
+//
+
+import Cocoa
+
+class view2: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Fake ID Final/editPhoto.swift
@@ -0,0 +1,13 @@
+//
+//  editPhoto.swift
+//  Fake ID Final
+//
+//  Created by Trevor Antram on 3/15/16.
+//  Copyright © 2016 Trevor Antram. All rights reserved.
+//
+
+import Cocoa
+
+class editPhoto: UIViewController {
+
+}
